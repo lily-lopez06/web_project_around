fe641278a31b8ad8b5d7a6eac10b2563e23e7481
Lily <liliana.lopez0610@gmail.com>
2024-12-28 16:38:59 -0600

    "popup cambia el nombre"

--- script.js
+++ script.js
@@ -6,29 +6,41 @@ document.addEventListener("DOMContentLoaded", () => {
   const saveProfileButton = document.getElementById("saveProfile");
   const nameInput = document.getElementById("nameInput");
   const aboutInput = document.getElementById("aboutInput");
+  const profileName = document.querySelector(".profile__name");
+  const profileParagraph = document.querySelector(".profile__paragraph");
 
   editProfileButton.addEventListener("click", () => {
     popup.classList.add("popup_opened");
+    nameInput.value = profileName.textContent;
+    aboutInput.value = profileParagraph.textContent;
+    popup.style.display = "block";
   });
 
   closePopupButton.addEventListener("click", () => {
     popup.classList.remove("popup_opened");
+    popup.style.display = "none";
   });
 
   addProfileButton.addEventListener("click", () => {
     popup.classList.add("popup_opened");
+    popup.style.display = "block";
   });
 
   saveProfileButton.addEventListener("click", () => {
     const name = nameInput.value;
     const about = aboutInput.value;
+    profileName.textContent = nameInput.value || "Nombre predeterminado";
+    profileParagraph.textContent =
+      aboutInput.value || "Descripción predeterminada";
+
+    popup.style.display = "none";
 
     if (name) {
-      document.querySelector(".profile-info h2").textContent = name;
+      document.querySelector(".profile__name").textContent = name;
     }
 
     if (about) {
-      document.querySelector("profile-info p").textContent = about;
+      document.querySelector("profile__paragraph").textContent = about;
     }
 
     popup.classList.remove("popup_opened");
